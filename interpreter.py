edde68bc88cf744c524434021ff506b19daca6c8
Peter Romianowski <pero@cling.com>
2023-03-17 18:49:35 +0100

    Add basic `if` logic to the interpreter

--- interpreter.py
+++ interpreter.py
@@ -5,8 +5,12 @@ last_token = ""
 last_string = ""
 last_identifier = ""
 last_function = ""
+last_expression_type = ""
+last_expression_lhs = ""
 execute_operator_at_line_end = ""
 inside_string = ""
+inside_if = ""
+skip_to_end_token = ""
 
 var1_name = ""
 var1_value = ""
@@ -15,7 +19,14 @@ while True:
     c = stdin.buffer.read(1).decode()
     if not c:
         break
-    if inside_string == "true":
+    if skip_to_end_token == "true":
+        if c == "\n":
+            if token == "end":
+                skip_to_end_token = ""
+            token = ""
+        else:
+            token = token + c
+    elif inside_string == "true":
         if c == '"':
             last_string = token
             inside_string = ""
@@ -39,10 +50,37 @@ while True:
     elif c == '"':
         inside_string = "true"
     elif c == "=":
-        execute_operator_at_line_end = token
-        token = ""
+        if inside_if:
+            if token == "=":
+                token = ""
+                last_expression_type = "=="
+                if last_string:
+                    last_expression_lhs = last_string
+                    last_string = ""
+                elif last_identifier == var1_name:
+                    last_expression_lhs = var1_value
+                    last_identifier = ""
+                last_expression_lhs
+            else:
+                token = token + c
+        else:
+            execute_operator_at_line_end = token
+            token = ""
     elif c == " ":
-        pass
+        if token == "if":
+            inside_if = "true"
+        elif token == "then":
+            inside_if = ""
+            if last_expression_type == "==":
+                if last_string:
+                    if last_string == last_expression_lhs:
+                        pass
+                    else:
+                        skip_to_end_token = "true"
+                    last_string = ""
+        else:
+            last_identifier = token
+        token = ""
     elif c == "\n":
         if execute_operator_at_line_end == "=":
             var1_name = last_identifier
@@ -51,4 +89,3 @@ while True:
             last_string = ""
     else:
         token = token + c
-

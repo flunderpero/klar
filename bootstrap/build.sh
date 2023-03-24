5bbcb3207b80ee54c4e7acb16fcd9cadbb7960c7
Peter Romianowski <pero@cling.com>
2023-03-24 22:16:43 +0100

    Add stage3 bringing integers to the language

--- bootstrap/build.sh
+++ bootstrap/build.sh
@@ -3,19 +3,12 @@ set -e
 
 dir_name=$(dirname $0)
 
-echo "Building stage0 ..."
-echo "Building stage0 done."
-echo
-
-echo "Building stage1 ..."
-cd $dir_name/stage1
-bash build_compiler.sh
-cd ..
-echo "Building stage1 done."
-echo
-
-echo "Building stage2 ..."
-cd stage2
-bash build_compiler.sh
-echo "Building stage2 done."
-echo
+for stage in $dir_name/stage*; do
+    echo "Building $stage ..."
+    cd $dir_name/$stage
+    echo $(pwd)
+    bash build_compiler.sh
+    cd ..
+    echo "Building $stage done."
+    echo
+done

10acb2795e747f0de233ccd9cddfe6aa71b7a094
Peter Romianowski <pero@cling.com>
2023-04-19 19:28:10 +0200

    Stage5 requires a main function

--- bootstrap/stage6/compile.sh
+++ bootstrap/stage6/compile.sh
@@ -7,6 +7,18 @@ ulimit -s 65000
 
 src_file="$1"
 base_file=$(basename "$1")
+# If the source file does not contain a function declaration we 
+# surround it with a `fn main() void`. 
+if ! grep -q "fn main" "$src_file"; then
+    tmp_file="$build_dir/$base_file"
+    echo "fn main() i32 begin" > $tmp_file
+    echo "" >> $tmp_file
+    cat $src_file >> $tmp_file
+    echo "" >> $tmp_file
+    echo "return 0" >> $tmp_file
+    echo "end" >> $tmp_file
+    src_file=$tmp_file
+fi
 ir_file="${base_file%.tl}.ll"
 obj_file="${base_file%.tl}.o"
 out_file="$2"

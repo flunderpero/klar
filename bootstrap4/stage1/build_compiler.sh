5790f501c7446bba566bece1c4352d2e41a2870f
Peter Romianowski <pero@cling.com>
2023-12-19 19:11:51 +0100

    [bootstrap4/stage1] First steps writing a lexer in Klar

---
+++ bootstrap4/stage1/build_compiler.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+mkdir -p build
+
+# echo "Phase 1: Compile with stage0 compiler"
+../stage0/compile.sh klarc.kl build/klarc 
+
+# echo "Phase 2: Compile with itself"
+# bash compile.sh compiler.tl build/compiler

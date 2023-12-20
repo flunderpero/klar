dc185b96315607bc083f16f40e797aa21426c710
Peter Romianowski <pero@cling.com>
2023-12-20 22:55:30 +0100

    [bootstrap4/stage1] Finish first version of the lexer in Klar

--- bootstrap4/stage1/test.sh
+++ bootstrap4/stage1/test.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 set -e
+shopt -s nullglob
 
+echo "Building compiler ..."
+mkdir -p build
+bash build_compiler.sh
+
+echo "Testing lexer ..."
 bun ../stage0/compiler.ts lexer.kl build/lexer
 build/lexer
+
+echo "Testing all source files under ../tests/stage0 ..."
+mkdir -p build
+for test_file in ../tests/stage0/*.kl; do
+    test=$(basename "$test_file")
+    compiled="build/${test%.kl}"
+    echo "$test"
+    bash compile.sh "$test_file" "$compiled"
+done

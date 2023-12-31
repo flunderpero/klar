718a13aa4ac8c6f47e9b1e335bd76147578c0dc8
Peter Romianowski <pero@cling.com>
2023-12-31 14:19:48 +0100

    [bootstrap4/stage1] Parse single-expression blocks and module

--- bootstrap4/stage1/test.sh
+++ bootstrap4/stage1/test.sh
@@ -2,22 +2,21 @@
 set -e
 shopt -s nullglob
 
-# echo "Building compiler ..."
-# mkdir -p build
-# bash build_compiler.sh
+echo "Building compiler ..."
+mkdir -p build
+bash build_compiler.sh
 
-# echo "Testing lexer ..."
+echo "Testing specs ..."
 bun ../stage0/compiler.ts lexer.spec.kl build/lexer.spec.js --debug-errors
 build/lexer.spec.js $@
 
 bun ../stage0/compiler.ts parser.spec.kl build/parser.spec.js --debug-errors
 build/parser.spec.js $@
 
-# echo "Testing all source files under ../tests/stage0 ..."
-# mkdir -p build
-# for test_file in ../tests/stage0/*.kl; do
-#     test=$(basename "$test_file")
-#     compiled="build/${test%.kl}"
-#     echo "$test"
-#     bash compile.sh "$test_file" "$compiled"
-# done
+echo "Testing all source files under ../tests/stage0 ..."
+for test_file in ../tests/stage0/*.kl; do
+    test=$(basename "$test_file")
+    compiled="build/${test%.kl}"
+    echo "$test"
+    bash compile.sh "$test_file" "$compiled"
+done

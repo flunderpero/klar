839204c3e458d3133811978f06b09cf67761e6b5
Peter Romianowski <pero@cling.com>
2024-02-27 18:36:22 +0100

    [bootstrap4/stage1] Parse `Type?` and trait bounds

--- bootstrap4/stage1/test.sh
+++ bootstrap4/stage1/test.sh
@@ -15,7 +15,7 @@ build/parser.spec.js $@
 
 echo "Testing the_book.md ..."
 bun ../stage0/compiler.ts the_book.spec.kl build/the_book.spec.js --debug-errors
-build/the_book.spec.js $@
+build/the_book.spec.js $@ --bail
 
 echo "Testing all source files under ../tests/stage0 ..."
 for test_file in ../tests/stage0/*.kl; do

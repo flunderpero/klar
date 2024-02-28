26e9ae67ac56b57e9fd4368eeeb294634aec6209
Peter Romianowski <pero@cling.com>
2024-02-28 18:07:54 +0100

    [bootstrap4/stage1] Parse or-patterns inside other patterns

--- bootstrap4/stage1/test.sh
+++ bootstrap4/stage1/test.sh
@@ -2,28 +2,74 @@
 set -e
 shopt -s nullglob
 
-echo "Building compiler ..."
+compiler=0
+lexer=0
+parser=0
+the_book=0
+files=0
+
+if [ "$1" == "" ]; then
+    lexer=1
+    parser=1
+    the_book=1
+    files=1
+else
+    case $1 in
+        compiler)
+            compiler=1
+            ;;
+        lexer)
+            lexer=1
+            ;;
+        parser)
+            parser=1
+            ;;
+        the_book)
+            the_book=1
+            ;;
+        files)
+            files=1
+            ;;
+        *)
+            echo "Unknown test target: $1"
+            exit 1
+            ;;
+    esac
+fi
+
 mkdir -p build
-bash build_compiler.sh
+if [ "$compiler" -eq 1 ]; then
+    echo "Building compiler ..."
+    bash build_compiler.sh
+fi
 
-echo "Testing specs ..."
-bun ../stage0/compiler.ts lexer.spec.kl build/lexer.spec.js --debug-errors
-build/lexer.spec.js $@
+if [ "$lexer" -eq 1 ]; then
+    echo "Testing lexer ..."
+    bun ../stage0/compiler.ts lexer.spec.kl build/lexer.spec.js --debug-errors
+    build/lexer.spec.js $@
+fi
 
-bun ../stage0/compiler.ts parser.spec.kl build/parser.spec.js --debug-errors
-build/parser.spec.js $@
+if [ "$parser" -eq 1 ]; then
+    echo "Testing parser ..."
+    bun ../stage0/compiler.ts parser.spec.kl build/parser.spec.js --debug-errors
+    build/parser.spec.js $@
+fi
 
-echo "Testing the_book.md ..."
-bun ../stage0/compiler.ts the_book.spec.kl build/the_book.spec.js --debug-errors
-build/the_book.spec.js $@ --bail
+if [ "$the_book" -eq 1 ]; then
+    echo "Testing the_book.md ..."
+    bun ../stage0/compiler.ts the_book.spec.kl build/the_book.spec.js --debug-errors
+    build/the_book.spec.js $@ --bail
+fi
 
-echo "Testing all source files under ../tests/stage0 ..."
-for test_file in ../tests/stage0/*.kl; do
-    if [[ "$test_file" == *"compile_error"* ]]; then
-        continue
-    fi
-    test=$(basename "$test_file")
-    compiled="build/${test%.kl}"
-    echo "$test"
-    bash compile.sh "$test_file" "$compiled"
-done
+if [ "$files" -eq 1 ]; then
+    echo "Testing all source files under ../tests/stage0 ..."
+    for test_file in ../tests/stage0/*.kl; do
+        if [[ "$test_file" == *"compile_error"* ]]; then
+            continue
+        fi
+        test=$(basename "$test_file")
+        compiled="build/${test%.kl}"
+        echo "$test"
+        bash compile.sh "$test_file" "$compiled"
+    done
+fi
